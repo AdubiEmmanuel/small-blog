606f0f85814e1ed18dbb32920a6ed0b3b122e9f2
Emmanuel Adubi <emmanueladubi24@gmail.com>
2023-02-18 01:43:21 +0100

    added navbar and my image

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,22 @@
+const Home = () =>{
+    return(
+        <>
+        <div class="container text-center">
+  <div class="row">
+    <div class="col-lg-3 col-md-3">
+      
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="images/me.png" class="rounded mx-auto d-block" alt="Bro Emma" />
+    </div>
+    <div class="col-lg-3 col-md-3">
+     
+    </div>
+  </div>
+</div>
+ 
+        </>
+    )
+}
+
+export default Home
